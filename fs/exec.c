db98cc979d03ffaf9bb23c67ede9e5f5bbd9b462
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-04 00:31:57 +0800

    KernelSU: scope-minimized manual hooks - k3.10
    This refactors original KSU hooks to replace deep kernel function hooks with targeted hooks.
    This backports KernelSU pr#1657 and having pr#2084 elements (32-bit sucompat).
    It reduces the scope of kernel function interception and still maintains full fucntionality.
    
    This commit is a squash of the following:
    *	fs/exec: sys_execve: ksu_handle_execve_sucompat hook
    *	fs/exec: compat_sys_execve: ksu_handle_execve_sucompat hook
    	fs/open: sys_faccessat: ksu_handle_faccessat hook
    *	fs/read_write: sys_read: ksu_handle_sys_read hook
    *	fs/stat: sys_newfstatat: ksu_handle_stat hook
    *	fs/stat: sys_fstatat64: ksu_handle_stat hook
    *	drivers: input: input_event: ksu_handle_input_handle_event hook
    *	drivers: tty/pty.c: pts_unix98_lookup: ksu_handle_devpts hook
    
    references: KernelSU pr#1657, pr#2084
    	https://kernelsu.org/guide/how-to-integrate-for-non-gki.html
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1736,11 +1736,30 @@ int get_dumpable(struct mm_struct *mm)
 	return __get_dumpable(mm->flags);
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
+			       void *__never_use_argv, void *__never_use_envp,
+			       int *__never_use_flags);
+extern int ksu_handle_execve_ksud(const char __user *filename_user,
+			const char __user *const __user *__argv);
+#ifdef CONFIG_COMPAT
+extern int ksu_handle_compat_execve_ksud(const char __user *filename_user,
+			const compat_uptr_t __user *__argv);
+#endif
+#endif
+
 SYSCALL_DEFINE3(execve,
 		const char __user *, filename,
 		const char __user *const __user *, argv,
 		const char __user *const __user *, envp)
 {
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execve_ksud(filename, argv);
+	else
+		ksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
+#endif
 	struct filename *path = getname(filename);
 	int error = PTR_ERR(path);
 	if (!IS_ERR(path)) {
@@ -1754,6 +1773,12 @@ asmlinkage long compat_sys_execve(const char __user * filename,
 	const compat_uptr_t __user * argv,
 	const compat_uptr_t __user * envp)
 {
+#ifdef CONFIG_KSU // 32-bit sucompat and 32-on-64 support
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_compat_execve_ksud(filename, argv);
+	else
+		ksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
+#endif
 	struct filename *path = getname(filename);
 	int error = PTR_ERR(path);
 	if (!IS_ERR(path)) {

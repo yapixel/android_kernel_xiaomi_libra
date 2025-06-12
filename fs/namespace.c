8002ee6a45a1d0f421aac152673ab83220457a50
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-12 23:56:22 +0800

    BRICKPORT: sys_umount: refactor to extract path_umount
    this is a brickport.
    
    we have everything needed anyway.
    
    adapted from the following:
    `fs: add ksys_mount() helper; remove in-kernel calls to sys_mount()`
    - https://github.com/torvalds/linux/commit/312db1aa1dc7bff133d95c92efcc5e42b57cefa6
    `fs: refactor ksys_umount`
    - https://github.com/torvalds/linux/commit/41525f56e2564c2feff4fb2824823900efb3a39f
    `init: add an init_umount helper`
    - https://github.com/torvalds/linux/commit/09267defa36aaff6ff829bd2fc8b043ec151cc3e
    `fs: fix a struct path leak in path_umount`
    - https://github.com/torvalds/linux/commit/25ccd24ffd9119c452d711efa2604a7a0c35956e
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1329,6 +1329,34 @@ static inline bool may_mount(void)
 	return ns_capable(current->nsproxy->mnt_ns->user_ns, CAP_SYS_ADMIN);
 }
 
+// caller is responsible for flags being sane
+int path_umount(struct path *path, int flags)
+{
+	struct mount *mnt;
+	int retval;
+
+	if (!may_mount())
+		return -EPERM;
+
+	mnt = real_mount(path->mnt);
+	retval = -EINVAL;
+	if (path->dentry != path->mnt->mnt_root)
+		goto dput_and_out;
+	if (!check_mnt(mnt))
+		goto dput_and_out;
+	retval = -EPERM;
+	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
+		goto dput_and_out;
+
+	retval = do_umount(mnt, flags);
+dput_and_out:
+	/* we mustn't call path_put() as that would clear mnt_expiry_mark */
+	dput(path->dentry);
+	mntput_no_expire(mnt);
+out:
+	return retval;
+}
+
 /*
  * Now umount can handle mount points as well as block devices.
  * This is important for filesystems which use unnamed block devices.
@@ -1340,39 +1368,20 @@ static inline bool may_mount(void)
 SYSCALL_DEFINE2(umount, char __user *, name, int, flags)
 {
 	struct path path;
-	struct mount *mnt;
-	int retval;
 	int lookup_flags = 0;
+	int retval;
 
 	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
 		return -EINVAL;
-
-	if (!may_mount())
-		return -EPERM;
 
 	if (!(flags & UMOUNT_NOFOLLOW))
 		lookup_flags |= LOOKUP_FOLLOW;
 
 	retval = user_path_at(AT_FDCWD, name, lookup_flags, &path);
 	if (retval)
-		goto out;
-	mnt = real_mount(path.mnt);
-	retval = -EINVAL;
-	if (path.dentry != path.mnt->mnt_root)
-		goto dput_and_out;
-	if (!check_mnt(mnt))
-		goto dput_and_out;
-	retval = -EPERM;
-	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
-		goto dput_and_out;
-
-	retval = do_umount(mnt, flags);
-dput_and_out:
-	/* we mustn't call path_put() as that would clear mnt_expiry_mark */
-	dput(path.dentry);
-	mntput_no_expire(mnt);
-out:
-	return retval;
+		return retval;
+	
+	return path_umount(&path, flags);
 }
 
 #ifdef __ARCH_WANT_SYS_OLDUMOUNT
